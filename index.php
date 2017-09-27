be53b8975149249947a97f58a95637465845997b
Mikkel Mouridsen <mikkelmouridsen@me.com>
2017-09-27 22:49:38 +0200

    Added main page

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-light" id="mainNav">
+      <a class="navbar-brand" href="">Rybners LAN</a>
+      <ul class="nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Info</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Booking</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Kontakt</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Nyheder</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Log ind</a>
+      </li>
+    </ul>
+    </nav>
+
+    <div class="container">
+      <div class="row">
+      <div class="col1"></div>
+      <div class="col2" id="content"> 
+        <h1>Rybners LAN</h1>
+        <hr>
+        <div class="text float-left">
+          <p>
+          Rybners HTX LAN er en begivenhed der holder sted flere gange om året på 
+          rybners HTX. Formålet med det vores LAN er at bringe skolen sammen, og 
+          fjerne rammerne for alder. Det vil sige at vi holder et event for folk der har 
+          interesse for computer og computerspil, også selvom man går i 1 år kan man 
+          stadig godt spille med en fra 3 år.
+          <br><br>
+          Hvis du er ny inden for computerspil eller gerne vil være en del af det, så er 
+          Rybners LAN den perfekte metode at starte på. Fordi computerspil er ikke bare at 
+          skyde hinanden. det er et fællesskab af folk der er fuld af forskellige færdigheder.
+          <br><br>
+          Servere
+          Vi har en Discord server som kan blive brugt under begivenheden, for at 
+          kommunikere med andre der er med. Vores Discord server kan også blive 
+          brugt til hold som man laver til konkurrencerne.
+          </p>
+        </div>
+        <div class="vid float-right">
+          <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/gSJo-O-RvCM" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+      <div class="col3"></div>
+      </div>
+    </div>
+
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  </body>
+</html>
