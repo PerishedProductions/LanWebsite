7cb88c6d9458380d2ced1e0e2564bca7cc7299d4
Mikkel Mouridsen <mikkelmouridsen@me.com>
2017-09-28 20:34:54 +0200

    Fix

--- pages/login.php
+++ pages/login.php
@@ -4,19 +4,21 @@
 <div class="col2" id="content"> 
     <h1>Log ind</h1>
     <hr>
-
-    <form>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-        </div>
-        <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>  
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-
+    <div class="d-flex justify-content-center">
+    
+        <form>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>  
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    
+    </div>
 </div>
 <div class="col3"></div>
 </div>
