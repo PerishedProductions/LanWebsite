1eb3bc18c92b02417ddc086d47236c57723cf2ea
THEXG4MING <emilsolo@sol.dk>
2017-09-28 20:36:22 +0200

    Made home.php good

--- pages/home.php
+++ pages/home.php
@@ -6,18 +6,19 @@
   <hr>
   <div class="text float-left">
     <p>
-    Rybners HTX LAN er en begivenhed der holder sted flere gange om året på 
-    rybners HTX. Formålet med det vores LAN er at bringe skolen sammen, og 
+    Rybners HTX LAN er en begivenhed der finder sted flere gange om året på 
+    rybners HTX. Formålet med vores LAN er at bringe skolen sammen, og 
     fjerne rammerne for alder. Det vil sige at vi holder et event for folk der har 
-    interesse for computer og computerspil, også selvom man går i 1 år kan man 
-    stadig godt spille med en fra 3 år.
+    interesse for computer og computerspil, også selvom man er 1. års elev kan man 
+    stadig godt spille med en 3. års elev.
     <br><br>
     Hvis du er ny inden for computerspil eller gerne vil være en del af det, så er 
     Rybners LAN den perfekte metode at starte på. Fordi computerspil er ikke bare at 
-    skyde hinanden. det er et fællesskab af folk der er fuld af forskellige færdigheder.
+    skyde hinanden. Det er et fællesskab af folk der er fuld af forskellige færdigheder.
     <br><br>
-    Servere
-    Vi har en Discord server som kan blive brugt under begivenheden, for at 
+    Servers:
+    <br>
+    Vi har en <a href="https://discord.me/htx_it">Discord server</a> som kan blive brugt under begivenheden, for at 
     kommunikere med andre der er med. Vores Discord server kan også blive 
     brugt til hold som man laver til konkurrencerne.
     </p>
